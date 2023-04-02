afee3bedc8a27df17f82576755cf4ce076238f9e
luen2003 <100186050+21020776TaDinhLuong@users.noreply.github.com>
2023-04-02 10:27:26 +0700

    change some files

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ let id_ = 0 ;
 let users = [];
 
 export const getUsers = (req, res) => {
-    console.log(`Users in the database: ${users}`);
+    console.log(`news in the database: ${users}`);
 
     res.send(users);
 }
@@ -14,7 +14,7 @@ export const createUser = (req, res) => {
 
     users.push(userWithId);
     
-    console.log(`User ${user.title} added to the database.`);
+    console.log(`news ${user.title} added to the database.`);
 };
 
 export const getUser = (req, res) => {
@@ -24,13 +24,13 @@ export const getUser = (req, res) => {
 };
 
 export const deleteUser = (req, res) => { 
-    console.log(`user with id ${req.params.id} has been deleted`);
+    console.log(`news with id ${req.params.id} has been deleted`);
     
     users = users.filter((user) => user.id != req.params.id);
 };
 
 export const deleteUsers = (req,res) => {
-    console.log(`All users has been deleted`);
+    console.log(`All news has been deleted`);
 
     users = users.filter((user) => true);
 
@@ -43,5 +43,5 @@ export const updateUser =  (req,res) => {
     if (req.body.subtitle) user.subtitle = req.body.subtitle;
     if (req.body.link) user.link = req.body.link;
 
-    console.log(`username has been updated to ${req.body.title}.age has been updated to ${req.body.subtitle}`)
+    console.log(`title has been updated to ${req.body.title} and subtitle has been updated to ${req.body.subtitle}`)
 };
